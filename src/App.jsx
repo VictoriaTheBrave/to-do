07c0a85590b6e93fd295e44b691789487762ca0c
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2019-02-25 19:04:24 +0300

    feat: add card editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EditCard from './components/EditCard';
 
 class App extends Component {
-  state = {
-    user: null,
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+    };
+    this.toggleEditing = this.toggleEditing.bind(this);
+  }
+
+  toggleEditing() {
+    this.setState(prevState => ({ editing: !prevState.editing }));
   }
 
   render() {
+    const { editing } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -17,15 +27,19 @@ class App extends Component {
         <section className="body">
           <div className="todo">
             <h2>ToDo</h2>
+            <div id="todo" />
           </div>
-          <div>
+          <div className="inprogress">
             <h2>In Progress</h2>
+            <div id="inprogress" />
           </div>
-          <div>
+          <div className="done">
             <h2>Done</h2>
+            <div id="done" />
           </div>
-          <div className="todo">
-            <button type="button">Add new item</button>
+          <div className="edit-container">
+            <button type="button" onClick={this.toggleEditing.bind(this)}>Add new item</button>
+            {editing ? <EditCard closeCardEditing={this.toggleEditing} /> : null}
           </div>
         </section>
       </div>
