6c12faed62430b098e7dd2670a74c7afc9be9e27
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2019-02-25 17:49:05 +0300

    feat: add initial markup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    user: null,
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +14,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>ToDo Manager</h1>
         </header>
+        <section className="body">
+          <div className="todo">
+            <h2>ToDo</h2>
+          </div>
+          <div>
+            <h2>In Progress</h2>
+          </div>
+          <div>
+            <h2>Done</h2>
+          </div>
+          <div className="todo">
+            <button type="button">Add new item</button>
+          </div>
+        </section>
       </div>
     );
   }
