447f9d2f516f2b12964d1b387d23ceb329af2933
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2019-03-01 13:21:19 +0300

    feat: add styles to cards

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Column.css';
 
 export default function Column(props) {
   const { h2, componentClass, arr } = props;
   const cardsList = arr.map((item, i) => {
     return (
-      <div key={i + item.title}>
+      <div key={i + item.title} className="card">
         <h3>{item.title}</h3>
         <p>{item.description}</p>
         <img src={item.photo} alt={item.photo}/>
